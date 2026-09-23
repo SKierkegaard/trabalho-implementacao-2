3bc357ea493f7c2275f908fa9f2f71238463d771
Pedro Vitório Arouche Campos <srn.kierkegaard@protonmail.com>
2026-09-22 23:24:06 -0300

    .

--- gcofo.c
+++ gcofo.c
@@ -125,4 +125,3 @@ int gCofoDestroy(GCofo *gCofo) {
 
     return FALSE;
 }
-
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "gcofo.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 typedef struct {
@@ -7,6 +8,11 @@ typedef struct {
     int numSongs;
     float numPlays;
 } Album;
+
+void cleanBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
 
 int queryAlbumName(void *a, void *b) {
     Album *nameA = (Album *) a;
@@ -40,4 +46,89 @@ int queryAlbumNumPlays(void *a, void *b) {
     }
 
     return FALSE;
+}
+
+Album *albumCreate() {
+    Album *album = malloc(sizeof(Album));
+    if (album != NULL) {
+        printf("Digite o nome do álbum: ");
+        fgets(album->name, 30, stdin);
+
+        printf("Digite a quantidade de músicas do álbum: ");
+        scanf("%s", &album->numSongs);
+        cleanBuffer();
+
+        printf("Digite a quantidade de visualizações(em milhões) do álbum: ");
+        scanf("%f", &album->numPlays);
+        cleanBuffer();
+
+        return album;
+    }
+
+    return NULL;
+}
+
+int main() {
+    int maxItems;
+    printf("Digite a capacidade do gCofo: ");
+    scanf("%d", &maxItems);
+    cleanBuffer();
+
+    GCofo *gCofo = gCofoCreate(maxItems);
+    if (gCofo == NULL) {
+        printf("Não foi possível criar o gCofo!\n");
+
+        return 1;
+    }
+
+    int option;
+    int running = TRUE;
+
+    while(running) {
+        printf("\n#----- Lista de Álbums -----#\n");
+        printf("| 1 - Inserir álbum         |\n");
+        printf("| 2 - Remover álbum         |\n");
+        printf("| 3 - Consultar álbum       |\n");
+        printf("| 4 - Mostar primeiro álbum |\n");
+        printf("| 5 - Mostrar próximo álbum |\n");
+        printf("| 6 - Esvaziar lista        |\n");
+        printf("| 7 - Destruir lista(sair)  |\n");
+        printf("#---------------------------#\n");
+        printf("> ");
+
+        if (scanf("%d", &option) != 1) {
+            printf("Entrada inválida.\n");
+            cleanBuffer();
+            continue;
+        }
+        cleanBuffer();
+        printf("\n");
+
+        switch (option) {
+            case 1:
+                Album *album = albumCreate();
+                if (album != NULL) {
+                    if(gCofoInsert(gCofo, album)) {
+                        printf("Álbum adicionado com sucesso!\n");
+                    } else {
+                        printf("Não foi possível adicionar o álbum à lista!\n");
+                    }
+                } else {
+                    printf("Não foi possível adicionar o álbum à lista!\n");
+                    free(album);
+                }
+                break;
+
+            case 7:
+                if (gCofoDestroy(gCofo)) {
+                    printf("Lista destuída com sucesso!\nAdeus...\n");
+                    gCofo == NULL;
+                    running = FALSE;
+                } else {
+                    printf("Não foi possível destruir a lista!\n");
+                }
+        }
+    }
+
+    return 0;
 }
